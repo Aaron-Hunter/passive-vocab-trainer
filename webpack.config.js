425f12ea28b718d2060f15083dde16b857b78612
Aaron Hooper <aaronhoopermail@gmail.com>
2022-06-12 11:47:57 +1200

    Fix output file path

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,5 @@ const path = require("path");
 
 module.exports = {
 	entry: './popup/refresh-db.js',
-	output: {
-		path: path.resolve(__dirname, "addon"),
-		filename: "[name]/src/index.js"
-	}
+	output: './popup/index.js'
 }
