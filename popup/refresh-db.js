b08784517bb6cc845e42e9601097f1c507b55ee9
Aaron Hooper <aaronhoopermail@gmail.com>
2022-06-11 13:26:54 +1200

    Add toolbar button to activate translation on domain

--- popup/refresh-db.js
+++ popup/refresh-db.js
@@ -8,14 +8,22 @@ function refreshDatabase() {
 	clearStorage.then(fillDatabase(), console.error("Failed to refresh data: ${error}"));
 }
 
+
+/* Check if there is an existing database, create a database if not, then run translate.js */
+function activateTranslation() {
+
+}
+
 function listenForClicks() {
 	document.addEventListener("click", (e) => {
-		function reportError(error) {
-			console.error('Could not refresh database: ${error}');
-		}
 
+		if (e.target.taskList.contains("refresh")) {
+			refreshDatabase();
+		} 
+		else if (e.target.taskList.contains("activate")) {
+			activateTranslation();
+		}
 		
-		refreshDatabase();
 	})
 }
 
