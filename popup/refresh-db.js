156e1f04bf0d5acd74e9a779845d3f6733347cb4
Aaron Hooper <aaronhoopermail@gmail.com>
2022-06-13 23:40:52 +1200

    Add function to recursively find apkg files in anki folder

--- popup/refresh-db.js
+++ popup/refresh-db.js
@@ -1,7 +1,24 @@
-const ankiToJson = require('anki-to-json').default;
+const ankiToJson = require("anki-to-json").default;
+const fs = require("fs").promises;
+const path = require("path");
+
+function listenForClicks() {
+	document.addEventListener("click", (e) => {
+
+		if (e.target.taskList.contains("refresh")) {
+			refreshDatabase();
+		} 
+		else if (e.target.taskList.contains("activate")) {
+			activateTranslation();
+		}
+		
+	})
+}
 
 function fillDatabase() {
 	//ankiToJSON and push to storage
+	const ankiDir = path.join(__dirname, "..", "anki");
+	const apkgFiles = await findApkgFiles(ankiDir);
 }
 
 /* Clear storage, process anki files with ankiToJson and push to storage */
@@ -16,16 +33,25 @@ function activateTranslation() {
 
 }
 
-function listenForClicks() {
-	document.addEventListener("click", (e) => {
+async function findApkgFiles(folderName) {
+	let apkgFiles = [];
 
-		if (e.target.taskList.contains("refresh")) {
-			refreshDatabase();
-		} 
-		else if (e.target.taskList.contains("activate")) {
-			activateTranslation();
+	async function findFiles(folderName) {
+		const items = await fs.readdir(folderName, { withFileTypes: true });
+
+		for (item of items) {
+			if (item.isDirectory()) {
+				await findFiles(path.join(folderName, item.name))
+			} else {
+				if (path.extname(item.name) === ".apkg") {
+					salesFiles.push(path.join(folderName, item.name));
+				}
+			}
 		}
-		
-	})
+	}
+
+	await findFiles(folderName);
+
+	return apkgFiles;
 }
 
