f615880bdfa0a780c515bd306f4bf2fc517f5e98
Aaron Hooper <aaronhoopermail@gmail.com>
2022-06-19 20:11:03 +1200

    Add translation functionality

--- translate.js
+++ translate.js
@@ -1,16 +1,23 @@
-// Get all the p nodes in an HTMLCollection
+//Get all the p nodes
 const nativeParas = document.getElementsByTagName("p");
 
-// Iterate through the collection, extracting the text and passing to the translation function
+//Iterate through the collection of p nodes, extracting the text and passing to the translation function
 for (let i = 0; i < nativeParas.length; i++) {
 	let nativeStr = nativeParas[i].textContent;
 	let foreignStr = translateKnownWords(nativeStr);
 	nativeParas[i].nodeValue = foreignStr;
 }
 
+/*Parse a given string and translate the known words
+Input:  nativeStr  a string with all words in the native language
+Output:  mixedStr  a string with known words translated*/
 function translateKnownWords(nativeStr) {
-	const words = nativeStr.split();
-	words.forEach(word => {
+	//Split by space, comma, or period
+	const mixedWords = nativeStr.split(/[ ,.]+/);
+	for (let i = 0; i < mixedWords.length; i++) {
 		//check if the word is in the database of known words and if it is, replace the word with its translation
-	})
+		let getTranslation= browser.storage.local.get(mixedWords[i]);
+		getTranslation.then(() => {mixedWords[i] = getTranslation}, () => {/*ignore*/});
+	}
+	return mixedWords.join(' ');
 }
