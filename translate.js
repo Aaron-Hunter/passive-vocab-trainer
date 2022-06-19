99037395ca54e9c991d496e0e3a0d3cdf83c0ea8
Aaron Hooper <aaronhoopermail@gmail.com>
2022-06-19 19:26:11 +1200

    Rename variables to generalise language

--- translate.js
+++ translate.js
@@ -1,15 +1,15 @@
 // Get all the p nodes in an HTMLCollection
-const engParas = document.getElementsByTagName("p");
+const nativeParas = document.getElementsByTagName("p");
 
 // Iterate through the collection, extracting the text and passing to the translation function
-for (let i = 0; i < engParas.length; i++) {
-	let engStr = engParas[i].textContent;
-	let transStr = translateKnownWords(engStr);
-	engParas[i].nodeValue = transStr;
+for (let i = 0; i < nativeParas.length; i++) {
+	let nativeStr = nativeParas[i].textContent;
+	let foreignStr = translateKnownWords(nativeStr);
+	nativeParas[i].nodeValue = foreignStr;
 }
 
-function translateKnownWords(initStr) {
-	let words = initStr.split();
+function translateKnownWords(nativeStr) {
+	let words = nativeStr.split();
 	words.forEach(word => {
 		//check if the word is in the database of known words and if it is, replace the word with its translation
 	})
